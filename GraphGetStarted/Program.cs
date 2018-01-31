758c30dfe1e884ce449641a608e54ec1fd2fd64f
Robert Corrigan <rjcorrig@gmail.com>
2018-01-30 20:59:17 -0500

    Retarget to Gremlin.Net

--- GraphGetStarted/Program.cs
+++ GraphGetStarted/Program.cs
@@ -2,17 +2,37 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Reflection;
     using System.Runtime.Versioning;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Configuration;
-    using Microsoft.Azure.Documents;
-    using Microsoft.Azure.Documents.Client;
-    using Microsoft.Azure.Documents.Linq;
-    using Microsoft.Azure.Graphs;
-    using Microsoft.Azure.Graphs.Elements;
     using Newtonsoft.Json;
+    using Gremlin.Net.Driver;
+    using Gremlin.Net.Structure.IO.GraphSON;
+    using Newtonsoft.Json.Linq;
+
+    internal class RelationIdentifier
+    {
+        public static string GraphsonPrefix = "janusgraph";
+        public static string GraphsonBaseType = "RelationIdentifier";
+        public static string GraphsonType = GraphSONUtil.FormatTypeName(GraphsonPrefix, GraphsonBaseType);
+
+        public RelationIdentifier(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+
+    internal class RelationIdentifierReader : IGraphSONDeserializer
+    {
+        public dynamic Objectify(JToken graphsonObject, GraphSONReader reader)
+        {
+            var value = reader.ToObject(graphsonObject["value"]);
+            return new RelationIdentifier(value);
+        }
+    }
 
     /// <summary>
     /// Sample program that shows how to get started with the Graph (Gremlin) APIs for Azure Cosmos DB.
@@ -29,17 +49,28 @@
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-            string endpoint = builder["cosmosDBConnection:endpoint"];
-            string authKey = builder["cosmosDBConnection:authkey"];
+            string server = builder["gremlinConnection:server"];
+            if (!short.TryParse(builder["gremlinConnection:port"], out short port))
+            {
+                port = 8182;
+            }
 
             Console.WriteLine($"Running GraphGetStarted {((TargetFrameworkAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(TargetFrameworkAttribute), false)[0]).FrameworkName }");
 
-            using (DocumentClient client = new DocumentClient(
-                new Uri(endpoint),
-                authKey,
-                new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp }))
+            var graphSONReader = new GraphSON2Reader(new Dictionary<string, IGraphSONDeserializer>
+            {
+                {
+                    RelationIdentifier.GraphsonType, new RelationIdentifierReader()
+                }
+            });
+
+            using (GremlinClient client = new GremlinClient(
+                new GremlinServer(server, port),
+                mimeType: GremlinClient.GraphSON2MimeType,
+                graphSONReader: graphSONReader))
             {
                 Program p = new Program();
+
                 p.RunAsync(client).Wait();
             }
         }
@@ -47,18 +78,11 @@
         /// <summary>
         /// Run the get started application.
         /// </summary>
-        /// <param name="client">The DocumentDB client instance</param>
+        /// <param name="client">The Gremlin client instance</param>
         /// <returns>A Task for asynchronous execuion.</returns>
-        public async Task RunAsync(DocumentClient client)
+        public async Task RunAsync(GremlinClient client)
         {
-            Database database = await client.CreateDatabaseIfNotExistsAsync(new Database { Id = "graphdb" });
-
-            DocumentCollection graph = await client.CreateDocumentCollectionIfNotExistsAsync(
-                UriFactory.CreateDatabaseUri("graphdb"),
-                new DocumentCollection { Id = "graphcollz" },
-                new RequestOptions { OfferThroughput = 1000 });
-
-            // Azure Cosmos DB supports the Gremlin API for working with Graphs. Gremlin is a functional programming language composed of steps.
+            // JanusGraph supports the Gremlin API for working with Graphs. Gremlin is a functional programming language composed of steps.
             // Here, we run a series of Gremlin queries to show how you can add vertices, edges, modify properties, perform queries and traversals
             // For additional details, see https://aka.ms/gremlin for the complete list of supported Gremlin operators
             Dictionary<string, string> gremlinQueries = new Dictionary<string, string>
@@ -68,55 +92,42 @@
                 { "AddVertex 2",    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)" },
                 { "AddVertex 3",    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')" },
                 { "AddVertex 4",    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')" },
-                { "AddEdge 1",      "g.V('thomas').addE('knows').to(g.V('mary'))" },
-                { "AddEdge 2",      "g.V('thomas').addE('knows').to(g.V('ben'))" },
-                { "AddEdge 3",      "g.V('ben').addE('knows').to(g.V('robin'))" },
-                { "UpdateVertex",   "g.V('thomas').property('age', 44)" },
+                { "AddEdge 1",      "g.V().has('id','thomas').addE('knows').to(g.V().has('id','mary'))" },
+                { "AddEdge 2",      "g.V().has('id','thomas').addE('knows').to(g.V().has('id','ben'))" },
+                { "AddEdge 3",      "g.V().has('id','ben').addE('knows').to(g.V().has('id','robin'))" },
+                { "UpdateVertex",   "g.V().has('id','thomas').property('age', 44)" },
                 { "CountVertices",  "g.V().count()" },
                 { "Filter Range",   "g.V().hasLabel('person').has('age', gt(40))" },
                 { "Project",        "g.V().hasLabel('person').values('firstName')" },
                 { "Sort",           "g.V().hasLabel('person').order().by('firstName', decr)" },
-                { "Traverse",       "g.V('thomas').out('knows').hasLabel('person')" },
-                { "Traverse 2x",    "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person')" },
-                { "Loop",           "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()" },
-                { "DropEdge",       "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()" },
+                { "Traverse",       "g.V().has('id','thomas').out('knows').hasLabel('person')" },
+                { "Traverse 2x",    "g.V().has('id','thomas').out('knows').hasLabel('person').out('knows').hasLabel('person')" },
+                { "Loop",           "g.V().has('id','thomas').repeat(out()).until(has('id', 'robin')).path()" },
+                { "DropEdge",       "g.V().has('id','thomas').outE('knows').where(inV().has('id', 'mary')).drop()" },
                 { "CountEdges",     "g.E().count()" },
-                { "DropVertex",     "g.V('thomas').drop()" },
+                { "DropVertex",     "g.V().has('id','thomas').drop()" },
             };
 
             foreach (KeyValuePair<string, string> gremlinQuery in gremlinQueries)
             {
                 Console.WriteLine($"Running {gremlinQuery.Key}: {gremlinQuery.Value}");
 
-                // The CreateGremlinQuery method extensions allow you to execute Gremlin queries and iterate
-                // results asychronously
-                IDocumentQuery<dynamic> query = client.CreateGremlinQuery<dynamic>(graph, gremlinQuery.Value);
-                while (query.HasMoreResults)
+                var query = await client.SubmitAsync<object>(gremlinQuery.Value);
+                foreach (var result in query)
                 {
-                    foreach (dynamic result in await query.ExecuteNextAsync())
-                    {
-                        Console.WriteLine($"\t {JsonConvert.SerializeObject(result)}");
-                    }
+                    Console.WriteLine($"\t {JsonConvert.SerializeObject(result)}");
                 }
 
                 Console.WriteLine();
             }
 
-            // Data is returned in GraphSON format, which be deserialized into a strongly-typed vertex, edge or property class
-            // The following snippet shows how to do this
-            string gremlin = gremlinQueries["AddVertex 1"];
-            Console.WriteLine($"Running Add Vertex with deserialization: {gremlin}");
-
-            IDocumentQuery<Vertex> insertVertex = client.CreateGremlinQuery<Vertex>(graph, gremlinQueries["AddVertex 1"]);
-            while (insertVertex.HasMoreResults)
+            string gremlin = gremlinQueries["AddVertex 1"] + ".valueMap()";
+            Console.WriteLine($"Running Add Vertex with valueMap: {gremlin}");
+            var valueMaps = await client.SubmitAsync<dynamic>(gremlin);
+            foreach (var valueMap in valueMaps)
             {
-                foreach (Vertex vertex in await insertVertex.ExecuteNextAsync<Vertex>())
-                {
-                    // Since Gremlin is designed for multi-valued properties, the format returns an array. Here we just read
-                    // the first value
-                    string name = (string)vertex.GetVertexProperties("firstName").First().Value;
-                    Console.WriteLine($"\t Id:{vertex.Id}, Name: {name}");
-                }
+                // Gremlin is designed for multi-valued properties, each value is an array
+                Console.WriteLine($"\t {JsonConvert.SerializeObject(valueMap)}");
             }
 
             Console.WriteLine();
